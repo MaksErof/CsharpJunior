d479dfce0556defeb5e251ff765bdbe6b7cc392a
Maks <75748966+MaksErof@users.noreply.github.com>
2021-05-16 17:12:26 +0300

    added task one

---
+++ variables/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberLesson = 1;
+            uint maxVariable = 4294967295;
+            double wakeUpTime = 8.00;
+            float timeToSleep = 22.00f;
+            string greeting = "Hello";
+            bool winter = false;
+            char gameWall = '@';
+            int channelTv = 5;
+            double numberPi = 3.14;
+            string nameWife = "Evgeniya";
+
+             
+        }
+    }
+}
