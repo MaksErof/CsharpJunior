4dc5578f9629e48ff6dd7dce985435aaf7f56f06
Maks <75748966+MaksErof@users.noreply.github.com>
2021-05-20 20:43:56 +0300

    third task

--- hospital/hospital/Program.cs
+++ hospital/hospital/Program.cs
@@ -10,11 +10,13 @@ namespace hospital
     {
         static void Main(string[] args)
         {
-            int timeOfReceipt = 10;
+            double timeOfReceipt = 10;
             Console.WriteLine("Введите количество человек перед вами в очереди");
-            int numbersHumans = Convert.ToInt32(Console.ReadLine());
-            int waitingTime = numbersHumans * timeOfReceipt;
-            Console.WriteLine($"Вы отстоите в очереди {waitingTime} минут");
+            double numbersHumans = Convert.ToDouble(Console.ReadLine());
+            double waitingTime = numbersHumans * timeOfReceipt / 60;
+            double waitingTimeHours = Math.Truncate(waitingTime);
+            double waitingTimeMinutes = (waitingTime - (waitingTimeHours)) * 60;
+            Console.WriteLine($"Вы отстоите в очереди {waitingTimeHours} часа и {waitingTimeMinutes} минут");
             Console.ReadKey();
 
         }
