5867212d085b7210635d837142a66e56c87ea2da
Maks <75748966+MaksErof@users.noreply.github.com>
2021-05-20 19:32:48 +0300

    third task

---
+++ hospital/hospital/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeOfReceipt = 10;
+            Console.WriteLine("Введите количество человек перед вами в очереди");
+            int numbersHumans = Convert.ToInt32(Console.ReadLine());
+            int waitingTime = numbersHumans * timeOfReceipt;
+            Console.WriteLine($"Вы отстоите в очереди {waitingTime} минут");
+            Console.ReadKey();
+
+        }
+    }
+}
