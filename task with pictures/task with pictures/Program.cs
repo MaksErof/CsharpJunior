70c7b4fd6d90fd0c95a8c1ac97d78203b5b173c5
Maks <75748966+MaksErof@users.noreply.github.com>
2021-05-20 07:40:24 +0300

    new task

---
+++ task with pictures/task with pictures/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_with_pictures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPictures = 52;
+            int picturesInRow = 3;
+            int rowOfPictures = numberOfPictures / picturesInRow;
+            int remainingPictures = numberOfPictures - rowOfPictures * picturesInRow;
+            Console.WriteLine("Из 52 картинок создастся " + rowOfPictures + " рядов");
+            Console.WriteLine(remainingPictures + " картинка останется");
+            Console.ReadKey();
+        }
+    }
+}
