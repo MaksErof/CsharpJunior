fe0b001bf2c45468b8aa9d002792eae53eb7c08e
Maks <75748966+MaksErof@users.noreply.github.com>
2021-05-21 09:32:27 +0300

    fourth task

---
+++ questions/questions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Давайте познакомимся, как вас зовут?");
+            string questionAboutName = Console.ReadLine();
+            Console.WriteLine("Какой у вас знак зодиака?");
+            string questionAboutZodiac = Console.ReadLine();
+            Console.WriteLine("Где вы работаете?");
+            string questionAboutJob = Console.ReadLine();
+            Console.WriteLine($"Вас зовут {questionAboutName}, вы по знаку зодиака {questionAboutZodiac}, ваша работа {questionAboutJob}. ");
+            Console.ReadKey();
+        }
+    }
+}
