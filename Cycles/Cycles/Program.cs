1f47e4ae180f0f1c6d609c01bc107289723e2dd4
Maks <75748966+MaksErof@users.noreply.github.com>
2021-05-23 20:22:56 +0300

    fifth task

---
+++ Cycles/Cycles/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cycles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberMessage;
+            string userMessage;
+            Console.WriteLine("Задайте желаемое количество повторений");
+            numberMessage = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите сообщение которое хотите вывести в консоль");
+            userMessage = Console.ReadLine();
+            for (int i = 1; i < numberMessage; i++)
+            {
+                Console.WriteLine(userMessage);
+            }
+            Console.ReadKey();
+        }
+    }
+}
