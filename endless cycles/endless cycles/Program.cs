02b5728328134b3af619348d42c5893305aa450b
Maks <75748966+MaksErof@users.noreply.github.com>
2021-05-27 19:39:24 +0300

    seventh task

---
+++ endless cycles/endless cycles/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace endless_cycles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string goingCycles;                                                                    
+            for (int i = 1; ; i++ )
+            {
+                Console.WriteLine("Для продолжения цикла введите 1, для выхода 2");
+                goingCycles = Console.ReadLine();
+                if (goingCycles == "1")
+                {
+                    Console.WriteLine(i + " цикл");
+                }
+                else if (goingCycles == "2")
+                {
+                    break;
+                    
+                }
+                
+                                                                           
+            }
+            Console.WriteLine("Вы вышли из цикла");
+            Console.ReadKey();
+        }
+
+    }
+}
