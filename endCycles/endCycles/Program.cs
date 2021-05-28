59e1b249426fadb381287245f1356c58b1dd2931
Maks <75748966+MaksErof@users.noreply.github.com>
2021-05-28 08:58:36 +0300

    seventh task

---
+++ endCycles/endCycles/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace endCycles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("для выхода из  цикла 1, для продолжения 2");
+            string goingCycles = Console.ReadLine();
+            int numberCycles = 1;
+
+            while (goingCycles == "2")
+            {
+                Console.WriteLine("для выхода из  цикла 1, для продолжения 2");
+                goingCycles = Console.ReadLine();
+                numberCycles++;
+                Console.WriteLine(numberCycles + " цикл");
+            }
+            Console.WriteLine("Вы вышли из цикла");
+            Console.ReadKey();
+
+        }
+    }
+}
