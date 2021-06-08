8b89ba8abdaed97cad763259c18b85922c556091
Maks <75748966+MaksErof@users.noreply.github.com>
2021-06-08 09:19:32 +0300

    tenth task

---
+++ changeNumber/changeNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace changeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberToChange;
+            for (numberToChange = 7; numberToChange <= 98; numberToChange += 7)
+            {
+                Console.WriteLine(numberToChange);
+            }
+            Console.ReadKey();
+        }
+    }
+}
