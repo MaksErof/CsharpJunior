c06f989c11ababa64a7dd564f089352f215fe9b8
Maks <75748966+MaksErof@users.noreply.github.com>
2021-06-09 21:33:13 +0300

    nine task

--- menu/menu/Program.cs
+++ menu/menu/Program.cs
@@ -15,30 +15,27 @@ namespace menu
             int userPassword;
             string consoleColor;
             int consoleSize;
-            int consoleSizeWidth;
-            
 
             while (menuCreation != "exit")
-            {                
+            {   
+                
                 Console.WriteLine("Для изменения ввода имени введите 1, для ввода пароля 2\nИзменить цвет консоли можно перейдя в меню 3\nМеню 4 позволит изменить размеры консоли\nДля выхода введите exit");
                 menuCreation = Console.ReadLine();
                 
                 switch (menuCreation)
                 {
-                    case ("1"):
 
+                    case ("1"):
                         Console.WriteLine("Введите логин, допускаются английский и русский ввод");
                         userLogin = Console.ReadLine();
                         break;
 
                     case ("2"):
-
                         Console.WriteLine("Введите пароль, пароль должен состоять из цифр");
                         userPassword = Convert.ToInt32(Console.ReadLine());
                         break;
 
                     case ("3"):
-
                         Console.WriteLine("Выберите цвет консоли\n1 красный цвет\n2 жёлтый цвет\n3 серый цвет");
                         consoleColor = Console.ReadLine();
 
@@ -60,7 +57,6 @@ namespace menu
                         break;
 
                     case ("4"):
-
                         Console.WriteLine("Выберите высоту и ширину консоли");
                         consoleSize = Convert.ToInt32(Console.ReadLine());
                         Console.WindowHeight = consoleSize;                        
