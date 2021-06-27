c25a2e60574f2c69d4e6cfaa6bbda1947ea05fb4
Maks <75748966+MaksErof@users.noreply.github.com>
2021-06-27 11:04:33 +0300

    array task two

--- max element/max element/Program.cs
+++ max element/max element/Program.cs
@@ -13,7 +13,7 @@ namespace max_element
             int numberOfColumns = 10;
             int numbersOfLines = 10;
             int[,] array = new int[numberOfColumns,numbersOfLines];
-            Random rand = new Random();
+            Random anyNumber = new Random();
             int maxElement = int.MinValue;                       
 
             Console.WriteLine("Начальная матрица\n");
@@ -22,46 +22,35 @@ namespace max_element
             {                
                 for (numbersOfLines = 0; numbersOfLines < array.GetLength(1); numbersOfLines++)
                 {
-                    array[numberOfColumns, numbersOfLines] = rand.Next(0, 20);
-                    Console.Write(array[numberOfColumns, numbersOfLines] + " ");                    
-                }
-
-                Console.WriteLine();                
-            }
-
-            Console.SetCursorPosition(0,13);
-
-            for(numberOfColumns = 0; numberOfColumns < array.GetLength(0); numberOfColumns++)
-            {
-                for (numbersOfLines = 0; numbersOfLines < array.GetLength(1); numbersOfLines++)
-                {
-                    if(maxElement < array[numberOfColumns, numbersOfLines])
+                    array[numberOfColumns, numbersOfLines] = anyNumber.Next(0, 101);
+                    Console.Write(array[numberOfColumns, numbersOfLines] + " ");
+                    if (maxElement < array[numberOfColumns, numbersOfLines])
                     {
                         maxElement = array[numberOfColumns, numbersOfLines];
                     }
-                }                
-            }
-            Console.Write("Максимальный элемент в матрице равен:");
-            Console.WriteLine(maxElement);
-            Console.WriteLine();
+                }
 
-            Console.WriteLine("Изменённая матрица\n");            
+                Console.WriteLine("");             
+            }
+
+            Console.Write("\nМаксимальный элемент в матрице равен:");
+            Console.WriteLine(maxElement + "\n");
+            
+            Console.WriteLine("Изменённая матрица\n");
 
             for (numberOfColumns = 0; numberOfColumns < array.GetLength(0); numberOfColumns++)
-            {                
+            {
                 for (numbersOfLines = 0; numbersOfLines < array.GetLength(1); numbersOfLines++)
-                {                    
-                    if (maxElement == array[numberOfColumns, numbersOfLines])
+                {
+                    if(maxElement == array[numberOfColumns, numbersOfLines])
                     {
                         array[numberOfColumns, numbersOfLines] = 0;
                     }
-
-                    Console.Write(array[numberOfColumns,numbersOfLines] + " ");
+                    Console.Write(array[numberOfColumns, numbersOfLines] + " ");
                 }
-
                 Console.WriteLine();
-            }
-
+            }                      
+            
             Console.ReadKey();
         }
     }
