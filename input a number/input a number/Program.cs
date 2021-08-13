25f5794fafbee9ffe7b9d77967afe7eb43722e5f
Maks <75748966+MaksErof@users.noreply.github.com>
2021-08-13 10:57:12 +0300

    methods third task

---
+++ input a number/input a number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace input_a_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNumber;
+
+            Console.WriteLine("Пользователь, введите число");
+            CheckTheNumber(out userNumber);
+            Console.WriteLine("Введено число");
+            Console.ReadKey();
+        }
+        static void CheckTheNumber(out int number)
+        {
+            while (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("Введено неверное число, впишите заново.");
+            }
+        }
+    }
+}
