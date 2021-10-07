26806642c91ee104798080cccf5b6a62790115d7
Maks <75748966+MaksErof@users.noreply.github.com>
2021-10-07 14:04:48 +0300

    collections dictionary

--- dictionary/dictionary/Program.cs
+++ dictionary/dictionary/Program.cs
@@ -13,12 +13,19 @@ namespace dictionary
             Dictionary<string, string> wordsAndValue = new Dictionary<string, string>();
             string userInput;
 
+            AddNewWord(wordsAndValue);
             Console.WriteLine("На текущий момент в словаре вы можете посмотреть значения следующих слов:");
             Console.WriteLine("Значение какого слова вы хотите узнать?");
             ShowAllWord(wordsAndValue);
             userInput = Console.ReadLine();
             Console.Clear();
             ShowValueWord(userInput, wordsAndValue);
+        }
+        static void AddNewWord(Dictionary<string, string> wordsAndValue)
+        {
+            wordsAndValue.Add("Модернизация", "Обновление объекта, приведение его в соответствие с новыми требованиями и нормами, техническими условиями, показателями качества.");
+            wordsAndValue.Add("Напутствие", "Слова, пожелания отправляющемуся в путь");
+            wordsAndValue.Add("Курьёз", "Странный, диковинный или смешной случай.");
         }
 
         static void ShowValueWord(string userInput, Dictionary <string, string> wordsAndValue)
@@ -45,10 +52,6 @@ namespace dictionary
         static void ShowAllWord(Dictionary<string, string> wordsAndValue)
         {
             int numberWord = 1;
-
-            wordsAndValue.Add("Модернизация", "Обновление объекта, приведение его в соответствие с новыми требованиями и нормами, техническими условиями, показателями качества.");
-            wordsAndValue.Add("Напутствие", "Слова, пожелания отправляющемуся в путь");
-            wordsAndValue.Add("Курьёз", "Странный, диковинный или смешной случай.");
 
             foreach (var item in wordsAndValue)
             {
