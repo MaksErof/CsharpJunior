bc610c6f435d1c27cee4f1aeec7b8523b0e7197a
Maks <75748966+MaksErof@users.noreply.github.com>
2021-10-08 14:57:34 +0300

    collection enter numbers user

---
+++ summ all numbers/summ all numbers/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace summ_all_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool enterNumbers = true;
+            string userInput;
+            List<int> listOfNumbers = new List<int>();
+
+            while (enterNumbers)
+            {
+                Console.WriteLine("Для добавления вводите числа, для сложения введите sum, для выхода введите exit");
+
+                switch (userInput = Console.ReadLine())
+                {
+                    default:
+                       int userNumber;
+                       while(int.TryParse(userInput, out userNumber))
+                       {
+                            AddNumbers(listOfNumbers, userNumber);
+                            break;
+                       }
+                        Console.Clear();
+                        break;
+
+                    case ("sum"):
+                        SumAllNumbers(listOfNumbers);
+                        break;
+
+                    case ("exit"):
+                        enterNumbers = false;
+                        break;
+                }
+            }
+        }
+
+        static void AddNumbers(List<int> listOfNumbers, int userInput)
+        {
+            listOfNumbers.Add(userInput);
+        }
+
+        static void SumAllNumbers(List<int> listOfNumbers)
+        {
+            int sumOfListNumbers = 0;
+            for (int i = 0; i<listOfNumbers.Count;i++)
+            {
+                sumOfListNumbers += listOfNumbers[i];
+            }
+
+            Console.WriteLine($"Сумма всех чисел в списке {sumOfListNumbers}");
+        }
+    }
+}
---
+++ summ all numbers/summ all numbers/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("summ all numbers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("summ all numbers")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("1ba40343-db4e-453a-86d2-0cec3d252d6d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
