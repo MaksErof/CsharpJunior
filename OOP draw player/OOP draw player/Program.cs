02e16a66d857ee931380bf72e62b34a4f7d06139
Maks <75748966+MaksErof@users.noreply.github.com>
2021-10-24 13:53:24 +0300

    draw player

---
+++ OOP draw player/OOP draw player/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_draw_player
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RenderingPlayer rendering = new RenderingPlayer();
+            Player positionOfPlayer = new Player(55,15);
+
+            rendering.DrawPlayer(positionOfPlayer.PlayerX,positionOfPlayer.PlayerY);
+           
+            Console.ReadKey();
+        }
+    }
+
+    class RenderingPlayer
+    {
+        public void DrawPlayer(int x, int y, char playerSymbol = '*')
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(playerSymbol);
+        }
+    }
+
+    class Player
+    {
+        public int PlayerX { get; private set; }
+        public int PlayerY { get; private set; }
+        public Player(int x,int y)
+        {
+            PlayerX = x;
+            PlayerY = y;
+        }
+    }
+}
